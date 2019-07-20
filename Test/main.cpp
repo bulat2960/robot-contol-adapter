517160866b1972e6d675784a809e9f88c5f10f27
Bulat Gubaydullin <gubaydullin.bulat7@gmail.com>
2019-07-21 00:01:08 +0300

    Change delay

--- Test/main.cpp
+++ Test/main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     dir.cd("System/release");
     QString pathToRcaExec = dir.path();
 
-    int waitTime = 100;
+    int waitTime = 200;
 
     auto res1 = QTest::qExec(new ConnectionTests(rcaIp, sceneIp, rcaPort, scenePort, pathToRcaExec, waitTime), argc, argv);
     auto res2 = QTest::qExec(new TransferTests(rcaIp, sceneIp, rcaPort, scenePort, pathToRcaExec, waitTime), argc, argv);
